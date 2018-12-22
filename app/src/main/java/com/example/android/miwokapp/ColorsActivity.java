3ec360048544ce3486d7333edacc479a7c21d1be
aga <a@a.a>
2018-12-22 07:34:09 +0100

    add clicklistener and intent to open numbers

--- app/src/main/java/com/example/android/miwokapp/ColorsActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.android.miwokapp;
-
-public class ColorsActivity {
-}
--- app/src/main/java/com/example/android/miwokapp/MainActivity.java
+++ app/src/main/java/com/example/android/miwokapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.miwokapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static android.view.View.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+
+        numbers.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
