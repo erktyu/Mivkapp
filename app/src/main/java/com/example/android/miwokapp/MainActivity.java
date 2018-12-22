58f1c8064736a428b877d0d4f567db7a2501cb47
aga <a@a.a>
2018-12-22 07:45:05 +0100

    Toast added new branch created

--- app/src/main/java/com/example/android/miwokapp/MainActivity.java
+++ app/src/main/java/com/example/android/miwokapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static android.view.View.*;
 
@@ -20,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(MainActivity.this, NumbersActivity.class);
+                Toast.makeText(v.getContext(), "numbers are open", Toast.LENGTH_SHORT).show();
                 startActivity(intent);
+
             }
         });
     }
